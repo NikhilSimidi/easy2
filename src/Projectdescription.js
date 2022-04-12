a88cbf93820a68aa3476ef5d31c8ac2749d46060
NikhilSimidi <94376560+NikhilSimidi@users.noreply.github.com>
2022-04-12 21:34:48 +0530

    none

--- src/Projectdescription.js
+++ src/Projectdescription.js
@@ -1,5 +1,9 @@
 import React from "react";
 function Projectdescription() {
-  return <div id="project-description">project-description</div>;
+  return (
+    <div id="project-description">
+      <p>project-description</p>
+    </div>
+  );
 }
 export default Projectdescription;
--- src/Projectname.js
+++ src/Projectname.js
@@ -1,5 +1,9 @@
 import React from "react";
 function Projectname() {
-  return <div id="project-name">project-name</div>;
+  return (
+    <div id="project-name">
+      <h1>project-name</h1>
+    </div>
+  );
 }
 export default Projectname;
